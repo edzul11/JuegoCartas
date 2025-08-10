adf906f27ad787affb31fd7614482006b56d766d
edzul11 <115831371+edzul11@users.noreply.github.com>
2025-08-09 22:35:40 -0400

    correccion de error tipo (?)
    en godot no se debe agregar el simbolo ? genera error.

--- scripts/Board.gd
+++ scripts/Board.gd
@@ -5,24 +5,24 @@ var monster_slots: Array = []
 var spell_slots: Array = []
 
 func _init():
-    # Two players: index 0 player, 1 AI
-    monster_slots = [[], []]
-    spell_slots = [[], []]
-    for i in range(3):
-        monster_slots[0].append(Slot.new("monster"))
-        monster_slots[1].append(Slot.new("monster"))
-        spell_slots[0].append(Slot.new("spell"))
-        spell_slots[1].append(Slot.new("spell"))
+	# Two players: index 0 player, 1 AI
+	monster_slots = [[], []]
+	spell_slots = [[], []]
+	for i in range(3):
+		monster_slots[0].append(Slot.new("monster"))
+		monster_slots[1].append(Slot.new("monster"))
+		spell_slots[0].append(Slot.new("spell"))
+		spell_slots[1].append(Slot.new("spell"))
 
 func get_monster_slot(player_index: int, column: int) -> Slot:
-    return monster_slots[player_index][column]
+	return monster_slots[player_index][column]
 
 func get_spell_slot(player_index: int, column: int) -> Slot:
-    return spell_slots[player_index][column]
+	return spell_slots[player_index][column]
 
 func get_free_slot(player_index: int, type: String) -> Slot:
-    var arr = type == "monster" ? monster_slots[player_index] : spell_slots[player_index]
-    for s in arr:
-        if s.is_empty():
-            return s
-    return null
+	var arr = monster_slots[player_index] if type == "monster" else spell_slots[player_index]
+	for s in arr:
+		if s.is_empty():
+			return s
+	return null
--- scripts/Game.gd
+++ scripts/Game.gd
@@ -1,6 +1,7 @@
 extends Node
 class_name Game
 
+
 var card_definitions: Dictionary = {}
 var players: Array = []
 var board: Board
@@ -18,150 +19,150 @@ var board_monsters_ai_container
 var board_spells_ai_container
 
 func _ready():
-    load_cards()
-    setup_game()
-    build_ui()
-    turn_manager.start_turn()
-    refresh_all()
+	load_cards()
+	setup_game()
+	build_ui()
+	turn_manager.start_turn()
+	refresh_all()
 
 func load_cards():
-    var file = FileAccess.open("res://data/cards.json", FileAccess.READ)
-    var data = JSON.parse_string(file.get_as_text())
-    for d in data:
-        card_definitions[d.id] = d
+	var file = FileAccess.open("res://data/cards.json", FileAccess.READ)
+	var data = JSON.parse_string(file.get_as_text())
+	for d in data:
+		card_definitions[d.id] = d
 
 func create_card(id: String) -> CardModel:
-    var data = card_definitions[id]
-    var card
-    if data.type == "monster":
-        card = MonsterCardModel.new()
-    else:
-        card = SpellCardModel.new()
-    card.from_dict(data.duplicate())
-    return card
+	var data = card_definitions[id]
+	var card
+	if data.type == "monster":
+		card = MonsterCardModel.new()
+	else:
+		card = SpellCardModel.new()
+	card.from_dict(data.duplicate())
+	return card
 
 func load_deck(path: String) -> Array:
-    var file = FileAccess.open(path, FileAccess.READ)
-    var data = JSON.parse_string(file.get_as_text())
-    return data
+	var file = FileAccess.open(path, FileAccess.READ)
+	var data = JSON.parse_string(file.get_as_text())
+	return data
 
 func setup_game():
-    board = Board.new()
-    var deck_player = load_deck("res://data/deck_player.json")
-    var deck_ai = load_deck("res://data/deck_ai.json")
-    var player = Player.new(self, deck_player, board, 0)
-    var ai = AIPlayer.new(self, deck_ai, board, 1)
-    players = [player, ai]
-    turn_manager = TurnManager.new(self)
+	board = Board.new()
+	var deck_player = load_deck("res://data/deck_player.json")
+	var deck_ai = load_deck("res://data/deck_ai.json")
+	var player = Player.new(self, deck_player, board, 0)
+	var ai = AIPlayer.new(self, deck_ai, board, 1)
+	players = [player, ai]
+	turn_manager = TurnManager.new(self)
 
 func build_ui():
-    var ui = Control.new()
-    add_child(ui)
-    var vbox = VBoxContainer.new()
-    ui.add_child(vbox)
-    phase_label = Label.new()
-    vbox.add_child(phase_label)
-    energy_label = Label.new()
-    vbox.add_child(energy_label)
-    var hp_box = HBoxContainer.new()
-    vbox.add_child(hp_box)
-    hp_label_player = Label.new()
-    hp_box.add_child(hp_label_player)
-    hp_label_ai = Label.new()
-    hp_box.add_child(hp_label_ai)
-    var board_box = VBoxContainer.new()
-    vbox.add_child(board_box)
-    board_monsters_ai_container = HBoxContainer.new()
-    board_box.add_child(board_monsters_ai_container)
-    board_spells_ai_container = HBoxContainer.new()
-    board_box.add_child(board_spells_ai_container)
-    board_spells_player_container = HBoxContainer.new()
-    board_box.add_child(board_spells_player_container)
-    board_monsters_player_container = HBoxContainer.new()
-    board_box.add_child(board_monsters_player_container)
-    hand_container = HBoxContainer.new()
-    vbox.add_child(hand_container)
-    var buttons = HBoxContainer.new()
-    vbox.add_child(buttons)
-    var next_btn = Button.new()
-    next_btn.text = "Siguiente Fase"
-    buttons.add_child(next_btn)
-    next_btn.pressed.connect(_on_next_phase)
-    var end_btn = Button.new()
-    end_btn.text = "Fin Turno"
-    buttons.add_child(end_btn)
-    end_btn.pressed.connect(_on_end_turn)
+	var ui = Control.new()
+	add_child(ui)
+	var vbox = VBoxContainer.new()
+	ui.add_child(vbox)
+	phase_label = Label.new()
+	vbox.add_child(phase_label)
+	energy_label = Label.new()
+	vbox.add_child(energy_label)
+	var hp_box = HBoxContainer.new()
+	vbox.add_child(hp_box)
+	hp_label_player = Label.new()
+	hp_box.add_child(hp_label_player)
+	hp_label_ai = Label.new()
+	hp_box.add_child(hp_label_ai)
+	var board_box = VBoxContainer.new()
+	vbox.add_child(board_box)
+	board_monsters_ai_container = HBoxContainer.new()
+	board_box.add_child(board_monsters_ai_container)
+	board_spells_ai_container = HBoxContainer.new()
+	board_box.add_child(board_spells_ai_container)
+	board_spells_player_container = HBoxContainer.new()
+	board_box.add_child(board_spells_player_container)
+	board_monsters_player_container = HBoxContainer.new()
+	board_box.add_child(board_monsters_player_container)
+	hand_container = HBoxContainer.new()
+	vbox.add_child(hand_container)
+	var buttons = HBoxContainer.new()
+	vbox.add_child(buttons)
+	var next_btn = Button.new()
+	next_btn.text = "Siguiente Fase"
+	buttons.add_child(next_btn)
+	next_btn.pressed.connect(_on_next_phase)
+	var end_btn = Button.new()
+	end_btn.text = "Fin Turno"
+	buttons.add_child(end_btn)
+	end_btn.pressed.connect(_on_end_turn)
 
 func refresh_all():
-    update_ui()
-    refresh_hand()
-    refresh_board()
+	update_ui()
+	refresh_hand()
+	refresh_board()
 
 func update_ui():
-    phase_label.text = "Fase: %s" % turn_manager.get_phase()
-    var current = players[turn_manager.current_player_index]
-    energy_label.text = "Energia: %d/%d" % [current.energy, current.max_energy]
-    hp_label_player.text = "HP Jugador: %d" % players[0].hp
-    hp_label_ai.text = "HP IA: %d" % players[1].hp
+	phase_label.text = "Fase: %s" % turn_manager.get_phase()
+	var current = players[turn_manager.current_player_index]
+	energy_label.text = "Energia: %d/%d" % [current.energy, current.max_energy]
+	hp_label_player.text = "HP Jugador: %d" % players[0].hp
+	hp_label_ai.text = "HP IA: %d" % players[1].hp
 
 func refresh_hand():
-    for c in hand_container.get_children():
-        c.queue_free()
-    for card in players[0].hand.cards:
-        var node = CardNode.new()
-        node.set_card(card)
-        node.card_clicked.connect(_on_hand_card_clicked)
-        hand_container.add_child(node)
+	for c in hand_container.get_children():
+		c.queue_free()
+	for card in players[0].hand.cards:
+		var node = CardNode.new()
+		node.set_card(card)
+		node.card_clicked.connect(_on_hand_card_clicked)
+		hand_container.add_child(node)
 
 func refresh_board():
-    for c in board_monsters_ai_container.get_children(): c.queue_free()
-    for c in board_spells_ai_container.get_children(): c.queue_free()
-    for c in board_spells_player_container.get_children(): c.queue_free()
-    for c in board_monsters_player_container.get_children(): c.queue_free()
-    for i in range(3):
-        board_monsters_ai_container.add_child(_create_slot_display(board.get_monster_slot(1, i)))
-        board_spells_ai_container.add_child(_create_slot_display(board.get_spell_slot(1, i)))
-        board_spells_player_container.add_child(_create_slot_display(board.get_spell_slot(0, i)))
-        board_monsters_player_container.add_child(_create_slot_display(board.get_monster_slot(0, i)))
+	for c in board_monsters_ai_container.get_children(): c.queue_free()
+	for c in board_spells_ai_container.get_children(): c.queue_free()
+	for c in board_spells_player_container.get_children(): c.queue_free()
+	for c in board_monsters_player_container.get_children(): c.queue_free()
+	for i in range(3):
+		board_monsters_ai_container.add_child(_create_slot_display(board.get_monster_slot(1, i)))
+		board_spells_ai_container.add_child(_create_slot_display(board.get_spell_slot(1, i)))
+		board_spells_player_container.add_child(_create_slot_display(board.get_spell_slot(0, i)))
+		board_monsters_player_container.add_child(_create_slot_display(board.get_monster_slot(0, i)))
 
 func _create_slot_display(slot: Slot) -> Control:
-    var btn = Button.new()
-    if slot.card:
-        btn.text = slot.card.name
-    else:
-        btn.text = "-"
-    return btn
+	var btn = Button.new()
+	if slot.card:
+		btn.text = slot.card.name
+	else:
+		btn.text = "-"
+	return btn
 
 func _on_hand_card_clicked(card: CardModel):
-    var player = players[0]
-    var slot = board.get_free_slot(0, card.type)
-    if slot and card.cost <= player.energy:
-        player.play_card(card, slot, players[1])
-        refresh_all()
+	var player = players[0]
+	var slot = board.get_free_slot(0, card.type)
+	if slot and card.cost <= player.energy:
+		player.play_card(card, slot, players[1])
+		refresh_all()
 
 func _on_next_phase():
-    turn_manager.next_phase()
-    refresh_all()
+	turn_manager.next_phase()
+	refresh_all()
 
 func _on_end_turn():
-    if turn_manager.get_phase() != "END":
-        return
-    turn_manager.end_turn()
-    refresh_all()
-    if turn_manager.current_player_index == 1:
-        ai_turn()
+	if turn_manager.get_phase() != "END":
+		return
+	turn_manager.end_turn()
+	refresh_all()
+	if turn_manager.current_player_index == 1:
+		ai_turn()
 
 func ai_turn():
-    var ai = players[1]
-    turn_manager.current_phase_index = 1
-    update_ui()
-    ai.perform_main_phase(self)
-    refresh_board()
-    turn_manager.current_phase_index = 2
-    update_ui()
-    ai.perform_battle_phase(self)
-    refresh_board()
-    turn_manager.current_phase_index = 3
-    update_ui()
-    turn_manager.end_turn()
-    refresh_all()
+	var ai = players[1]
+	turn_manager.current_phase_index = 1
+	update_ui()
+	ai.perform_main_phase(self)
+	refresh_board()
+	turn_manager.current_phase_index = 2
+	update_ui()
+	ai.perform_battle_phase(self)
+	refresh_board()
+	turn_manager.current_phase_index = 3
+	update_ui()
+	turn_manager.end_turn()
+	refresh_all()
